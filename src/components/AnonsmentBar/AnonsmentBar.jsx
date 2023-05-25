a787175c81ada857717083adbc0ab3c012cc7850
pepsif <pepsi_fack@ukr.net>
2023-05-25 17:25:27 +0300

    feat: add animation row in Anonsment.jsx and edit Anonsment.css

--- src/components/AnonsmentBar/AnonsmentBar.jsx
+++ src/components/AnonsmentBar/AnonsmentBar.jsx
@@ -1,10 +1,12 @@
 import "./Anonsment.css";
 
+
+
 export const AnonsmentBar = () => {
   return (
     <section className="anonsment">
       <div className="container anonsment-container d-flex">
-        <div className="scroll-text">find best burger in your city </div>
+      <p className="anonsment-text">Сьогодні при замовленні з 11-00 до 12-30 діють знижки на всю продукцію 20%</p>
       </div>
     </section>
   );
