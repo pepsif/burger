e027d79b46b54d03a31f069f8b3623f01f306067
pepsif <pepsi_fack@ukr.net>
2023-11-08 10:08:05 +0200

    feat: edit

--- src/components/BurgerCard/Burger.tsx
+++ src/components/BurgerCard/Burger.tsx
@@ -25,7 +25,6 @@ export default function Burger({id, value, title, rating, price, background, ima
     const dispatch = useDispatch();
     // const activeBurgerId = useSelector(state => state.burgers.activeBurgerId);
 
-    console.log('fggfg',imageUrl)
     const style = {
         background: background,
 
